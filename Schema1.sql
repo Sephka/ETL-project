664cf8139b4da25cc74e54f0528e106afe5867b8
Jhon Perry <jhon.perry.2595@gmail.com>
2019-11-12 13:58:17 -0700

    Updated File no primary key

---
+++ Schema1.sql
@@ -0,0 +1,26 @@
+CREATE TABLE bitstamp (
+     currency VARCHAR(30),
+    date VARCHAR(30),
+    open NUMERIC(12, 2),
+    high NUMERIC(12, 2),
+    low NUMERIC(12, 2),
+    close NUMERIC(12, 2)
+);
+
+CREATE TABLE coinbase (
+    currency VARCHAR(30),
+    date VARCHAR(30),
+    open NUMERIC(12, 2),
+    high NUMERIC(12, 2),
+    low NUMERIC(12, 2),
+    close NUMERIC(12, 2)
+);
+
+CREATE TABLE eth (
+     currency VARCHAR(30),
+    date VARCHAR(30),
+    open NUMERIC(12, 2),
+    high NUMERIC(12, 2),
+    low NUMERIC(12, 2),
+    close NUMERIC(12, 2)
+);
